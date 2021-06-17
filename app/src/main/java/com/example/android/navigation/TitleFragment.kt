93e48c9dc35eae93e95e3caff23ee35aa39757d8
Rosy Sunuwar <>
2021-06-17 18:32:17 -0500

    chore: step7

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -25,7 +25,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
@@ -34,13 +33,16 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener (
-                // TODO (11) Replace action ID with actionTitleFragmentToGameFragment
-                // From TitleFragmentDirections
-                // We will have to switch this to using an anonymous function, since
-                // createNavigateOnClickListener cannot take NavDirections
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+            inflater, R.layout.fragment_title, container, false
+        )
+//        binding.playButton.setOnClickListener(
+//            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+//        )
+
+        binding.playButton.setOnClickListener { view ->
+            view.findNavController()
+                .navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        } // replacing above to use title fragment directions
         setHasOptionsMenu(true)
         return binding.root
     }
